b984f0f89df05d2fa738448b22dfaefe9aa9cc05
Ahmed Hussein <wolktheline@yahoo.com>
2020-01-24 22:24:53 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,15 +5,20 @@ attr_accessor :length1, :length2, :length3
    @length2 = length2
    @length3 = length3
  end 
- def kind 
-  :equilateral
-
-  :isosceles
-
-  :scalene
-end 
-
-
+ def kind()
+    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
+      raise TriangleError
+    elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+      raise TriangleError
+    else
+      if (@side_1 == @side_2) && (@side_2 == @side_3)
+        :equilateral
+      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+        :isosceles
+      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+        :scalene
+      end
+    end
 end 
 
 class TraingleError < StandatrdError 
