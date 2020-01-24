d2d98cf767df54241392eb28f370685f877956a3
Ahmed Hussein <wolktheline@yahoo.com>
2020-01-24 22:28:41 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,6 @@
 class Triangle
   # write code here	  # write code here
-end 	
+
   attr_accessor :side1, :side2, :side3
   @sides = []
 
@@ -48,6 +48,4 @@ end
 end
 
 class TriangleError < StandardError
-
-end
 end 
